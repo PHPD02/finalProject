e23af062f46b0db6db7d690ac55fac9389d17994
Wu Cheng Han <maxwu84@gmail.com>
2022-06-18 11:20:37 +0800

    先建立一個 順弁(sunsua) 基底網頁

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { HashRouter, Switch, Route} from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './chattyComponents/login';
+import Sunsua from './component/Sunsua/sunsua.jsx';
 
 class App extends Component {
-  state = {  } 
-  render() { 
+  state = {}
+  render() {
     return (
       <HashRouter>
         <div className='container'>
@@ -13,13 +14,14 @@ class App extends Component {
             {/* <Route path="/" component={Index} exact/> */}
             {/* <Route path ="/index" component={Index} /> */}
             <Route path="/login" component={Login} />
+            <Route path="/sunsua" component={Sunsua} />
           </Switch>
         </div>
       </HashRouter>
     );
   }
 }
- 
+
 export default App;
-  
+
 
---
+++ project/src/component/Sunsua/sunsua.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./css/sunsua.css";
+
+class Sunsua extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <h1>順弁</h1>
+            </div>
+
+        );
+    }
+}
+
+export default Sunsua;

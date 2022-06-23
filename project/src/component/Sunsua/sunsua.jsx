ed70d9ae895b51baf2e14cc1bfc9c59cd8da8484
Wu Cheng Han <maxwu84@gmail.com>
2022-06-23 13:24:30 +0800

    順弁 樣式 字體 改變

--- project/src/component/Sunsua/sunsua.jsx
+++ project/src/component/Sunsua/sunsua.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, HashRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Switch, Route, Link, NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import SunsuaProposal from './sunsuaProposal';
@@ -33,20 +33,16 @@ class Sunsua extends Component {
             <div id="sunsua" className='py-2'>
                 <Navbar></Navbar>
                 <div className='header mt-5'>
-                    <h1 >順弁</h1>
+                    <h1 className='h1' >順弁</h1>
                 </div>
                 <p></p>
                 <div className='container'>
-
                     <div className='row'>
                         {/* 左側 點選列表 */}
-                        <div className='col-2'>
-                            <div>
-                                <div className="sunsuaBtn" onClick={this.innerContentChange}><Link to="/sunsua/intro" >介紹</Link></div>
-                                <div className="sunsuaBtn" onClick={this.innerContentChange}><Link to="/sunsua/proposal" >提案</Link></div>
-                                <div className="sunsuaBtn" onClick={this.innerContentChange}><Link to="/sunsua/order" >搜方案</Link></div>
-                            </div>
-                            <hr />
+                        <div className='col-2 d-flex flex-column'>
+                            <div className="my-1 h2 font-weight-bold d-flex justify-content-center" onClick={this.innerContentChange}><NavLink to="/sunsua/intro" >介紹</NavLink></div>
+                            <div className="my-1 h2 font-weight-bold d-flex justify-content-center" onClick={this.innerContentChange}><Link to="/sunsua/proposal" >提案</Link></div>
+                            <div className="my-1 h2 font-weight-bold d-flex justify-content-center" onClick={this.innerContentChange}><Link to="/sunsua/order">搜方案</Link></div>
                         </div>
                         {/* 右側顯示列表 */}
                         <div className="col-10">
@@ -55,7 +51,7 @@ class Sunsua extends Component {
                     </div>
                 </div>
                 <div className='footer'>
-                    <p >about us</p>
+                    <p>about us</p>
                 </div>
 
             </div>

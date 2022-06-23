20ccba847bf83cd31966e06cdedd6ca65facf9a5
Rjie <linjj0709@gmail.com>
2022-06-23 16:53:56 +0800

    訂單詳情  靜態

--- finalProject/project/src/partpage/orderDetails.jsx
+++ finalProject/project/src/partpage/orderDetails.jsx
@@ -8,61 +8,68 @@ class orderDetails extends Component {
       <React.Fragment>
         <Navbar />
         <br />
-        <div className="container mt-5 ">
-          <div>
-            {/* 小標題 */}
-            <div>
-              <h1>訂單詳情</h1>
-            </div>
-            {/* 預約送達時間 */}
-            <div>
-              <p>預計送達時間</p>
-              <p>time</p>
-              <img src="#" />
-            </div>
-            <hr />
-            {/* 清單 */}
-            <table>
-              <tr>
-                <td>訂單編號：</td>
-                <td>12345678</td>
-              </tr>
-              <tr>
-                <td>外送餐廳：</td>
-                <td>八方雲集(雲集店)</td>
-              </tr>
-              <tr>
-                <td>送餐目的地：</td>
-                <td>公益路二段</td>
-              </tr>
-              <tr>
-                <td>總計：</td>
-                <td>$800</td>
-              </tr>
-            </table>
-            <hr />
-            <table>
-              <th>
-                <td>訂單內容</td>
-                <td>訂單數量</td>
-              </th>
-              <tr>
-                <td>韓式臘味</td>
-                <td>$50</td>
-              </tr>
-              <tr>
-                <td>韓式狗味</td>
-                <td>$8000</td>
-              </tr>
-            </table>
-            <hr />
+        <div className="mt-5 ">
+          <h1 className='text-center'>訂單詳情</h1>
+          <br />
+          <br />
+          <div className='container'>
             <div className='row'>
-              <div className=" text-center col-6">
-                <button>有需要幫助的嗎 ?</button>
+              <div className='col-3'></div>
+              <div className='col-4'>
+                <p>預計外送時間</p>
+                <p>時間</p>
+                <img src="#" /><br />
+                {/* 讀取外送時間 */}
+                <input type="range" name="points" min="1" max="10" />
+                {/* 再送達後更改內容 */}
+                <p>餐點準備中</p>
               </div>
-              <div className="col-6">
-                <button>客服中心</button>
-                <button>外送人員</button>
+              <div className='col-4'>
+                <p>預計外送時間</p>
+                <p>訂單編號: #1212141</p>
+                <p>訂單配送來自: 八方雲集(雲集店)</p>
+                <p>送餐地址: 公益路二段</p>
+                <p>總計: $500</p>
+                <p>查看細節(10個品項): <select><option>空</option></select></p>
+              </div>
+            </div>
+          </div>
+          {/* 撈資料庫產生 */}
+          <div className='col-1'></div>
+          <div className='row'>
+            <div className='col-7'></div>
+            <div className='col-5 row'>
+              <p><b>餐點內容</b></p>
+              <p className='ml-4'><b>數量</b></p>
+              <p className='ml-5'><b>金額</b></p>
+            </div>
+            <div className='col-7'></div>
+            <div className='col-5 row'>
+              <p><b>韓式臘味</b></p>
+              <p className='ml-5'>2</p>
+              <p className='ml-5'>$30</p>
+            </div>
+            <div className='col-7'></div>
+            <div className='col-5 row'>
+              <p><b>韓式狗味</b></p>
+              <p className='ml-5'>1</p>
+              <p className='ml-5'>$900</p>
+            </div>
+          </div>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-4 ml-5'></div>
+              <div className='col-3'>
+                <h4>需要幫助嗎 ?</h4>
+                <p className='ml-5'>有需要的話聯絡我們</p>
+              </div>
+            </div>
+          </div>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-6 ml-5'></div>
+              <div className='col-2'>
+                <button className="btn btn-outline-danger ml-1">客服中心</button>
               </div>
             </div>
           </div>

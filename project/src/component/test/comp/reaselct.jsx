41a9426758a2252223f712faac0660ebfca09e79
Wu Cheng Han <maxwu84@gmail.com>
2022-06-24 13:40:20 +0800

    reaselct 元件 class 版本 及 function 版本

--- project/src/component/test/comp/reaselct.jsx
+++ project/src/component/test/comp/reaselct.jsx
@@ -1,32 +1,73 @@
+
 import React, { Component ,useState} from 'react';
-
-// import { Select, AsyncSelect, CreatableSelect, AsyncCreatableSelect } from "chakra-react-select";
 import {Select,SelectOption,SelectInput} from 'reaselct'
+
 import CityCountyData from '../../../data/taiwan city country/CityCountyData.json'
+const CityArray = CityCountyData.map((value)=>value.CityName)
 
-
-function Reaselct (){
-    const [value, setValue] = useState([]);
-    const [city, setCity] = useState(['台北', '台中', '台南']);
+/* === class 版本 === */
+class Reaselct extends Component {
+  state = {
+    value: [],
+    animals: ['chicken', 'cow', 'mouse']
+  }
+  setValue = (v)=>{
+    this.state.value = v;
+    this.setState({});
+  }
+  setAnimals = (opts)=>{
+    this.state.animals = opts.map((o) => o.value);
+    this.setState({});
+  }
+  render() {
     return (
       <div style={{ width: 300 }}>
         <Select
           multiple
           closeOnSelect={false}
           createable
-          placeholder="請選擇地區"
-          value={value}
-          onChange={(v) => setValue(v)}
-          onOptionsChange={(opts) => setCity(opts.map((o) => o.value))}
+          placeholder="Add some categories or pick existing one..."
+          value={this.state.value}
+          onChange={(v) => this.setValue(v)}
+          onOptionsChange={(opts) => this.setAnimals(opts)}
         >
-          {city.map((o) => (
+          {this.state.animals.map((o) => (
             <SelectOption key={o} value={o}>
               {o}
             </SelectOption>
           ))}
         </Select>
-      </div>
-    );
+      </div>);
   }
-
+}
 export default Reaselct;
+
+
+/* === function 版本 === */
+
+// function Reaselct (){
+//   const [value, setValue] = useState([]);
+//   // const [city, setCity] = useState(['台北', '台中', '台南']);
+//   const [city, setCity] = useState(CityArray);
+//   return (
+//     <div style={{ width: 300 }}>
+//       <Select
+//         multiple
+//         closeOnSelect={false}
+//         createable
+//         placeholder="請選擇地區"
+//         value={value}
+//         onChange={(v) => setValue(v)}
+//         onOptionsChange={(opts) => setCity(opts.map((o) => o.value))}
+//       >
+//         {city.map((o) => (
+//           <SelectOption key={o} value={o}>
+//             {o}
+//           </SelectOption>
+//         ))}
+//       </Select>
+//     </div>
+//   );
+// }
+
+// export default Reaselct;

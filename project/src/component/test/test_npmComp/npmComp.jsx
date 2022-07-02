03933b88938d8f62639ae4797b8abc88c7630c54
Wu Cheng Han <maxwu84@gmail.com>
2022-07-02 11:26:34 +0800

    測試

--- project/src/component/test/test_npmComp/npmComp.jsx
+++ project/src/component/test/test_npmComp/npmComp.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import Countdown from 'react-countdown';
 
-const Completionist = () => <span>You are good to go!</span>;
+
 class Test extends Component {
     state = {}
     render() {
         return (
             <>
-                <Countdown date={Date.now() + 5000000}>
-                    <Completionist />
-                </Countdown>
             </>
         );
     }

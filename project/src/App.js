ab556629e64fb5c58009109b925d7fd462ccecc4
Wu Cheng Han <maxwu84@gmail.com>
2022-07-05 23:24:17 +0800

    刪除 測試檔案，及其他不屬於此分支之頁面

--- project/src/App.js
+++ project/src/App.js
@@ -2,19 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter, HashRouter, Switch, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import "../node_modules/font-awesome/css/font-awesome.min.css";
 
-import Firstpage from "./component/firstPage/firstpage";
-import OrderDetails from "./component/partpage/orderDetails";
-import ShopList from "./component/partpage/shopList";
-import Login from './chattyComponents/login';
 import Sunsua from './component/Sunsua/sunsua.jsx';
-
-
-/* 測試元件專區 */
-import PhpTest from './component/test/phptest/phptest';
-import TimeCountDown from './component/Sunsua/comp/TimeCountDown';
-import Test from './component/test/test_npmComp/npmComp';
 
 class App extends Component {
   state = {}
@@ -23,9 +12,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           {/* 首頁 */}
-          <Route path="/" component={Firstpage} exact />
           {/* 登入頁面 */}
-          <Route path="/login" component={Login} />
           {/* 順弁頁 */}
           <Route path="/sunsua" component={Sunsua} />
           {/* 外送頁 */}
@@ -33,15 +20,6 @@ class App extends Component {
 
           {/* ====================================== */}
 
-          <Route path="/firstpage" component={Firstpage} />
-          <Route path="/orderDetails" component={OrderDetails} />
-          <Route path="/shopList" component={ShopList} />
-
-
-          {/* ---------- 測試元件專區 ---------- */}
-          <Route path="/phptest" component={PhpTest} />
-          <Route path="/test/tcd" component={TimeCountDown} />
-          <Route path="/test/npmComp" component={Test} />
         </Switch>
       </BrowserRouter>
     );
--- project/src/component/test/comp_countdown-circle-timer/readme.md
+++
@@ -1,9 +0,0 @@
-
-
-# 文件 :
-https://www.npmjs.com/package/react-countdown-circle-timer
-
-## 
-```
-npm install reaselct
-```

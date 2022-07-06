a18011ceee775fdec80fe080bcddad33c55dd4f4
Wu Cheng Han <maxwu84@gmail.com>
2022-07-06 09:35:37 +0800

    先砍掉所有路由及元件，待merge

--- project/src/App.js
+++ project/src/App.js
@@ -2,24 +2,13 @@ import React, { Component } from 'react';
 import { HashRouter, Switch, Route} from 'react-router-dom';
 
 
-import Login from './chattyComponents/login';
-import FoodpandaPage from './kangComponent/foodpandapage';
-import Restaurants from './kangComponent/restaurants';
 
 class App extends Component {
   state = {  } 
   render() { 
     return (
       <HashRouter>
-        <div className='container'>
-          <Switch>
-            {/* <Route path="/" component={Index} exact/> */}
-            {/* <Route path ="/index" component={Index} /> */}
-            <Route path="/login" component={Login} />
-            <Route path="/foodpandapage" component={FoodpandaPage}/>
-            <Route path="/restaurants" component={Restaurants} />
-          </Switch>
-        </div>
+          <h1>Hello ,PHPD02</h1>
       </HashRouter>
     );
   }

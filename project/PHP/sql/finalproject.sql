baaeb3357727ca123e36bce843a594c6b94ee010
Chatty <bisa8002@gmail.com>
2022-07-07 17:07:50 +0800

    sql檔案

--- project/PHP/sql/finalproject.sql
+++ project/PHP/sql/finalproject.sql
@@ -16,6 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `restaurant`
+--
+
+DROP TABLE IF EXISTS `restaurant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `restaurant` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `tel` varchar(20) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `picture` varchar(500) DEFAULT NULL,
+  `經度` varchar(50) DEFAULT NULL,
+  `緯度` varchar(50) DEFAULT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+LOCK TABLES `restaurant` WRITE;
+/*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usermember`
 --
 
@@ -24,14 +57,14 @@ DROP TABLE IF EXISTS `usermember`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usermember` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(300) NOT NULL,
-  `password` varchar(300) NOT NULL,
-  `firstName` varchar(100) NOT NULL,
-  `lastName` varchar(100) NOT NULL,
-  `tel` int(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `firstName` varchar(140) NOT NULL,
+  `lastName` varchar(140) NOT NULL,
+  `tel` varchar(20) NOT NULL,
   `deposit` int(100) NOT NULL DEFAULT 0,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +73,7 @@ CREATE TABLE `usermember` (
 
 LOCK TABLES `usermember` WRITE;
 /*!40000 ALTER TABLE `usermember` DISABLE KEYS */;
-INSERT INTO `usermember` VALUES (4,'test@gmail.com','$2y$10$wgIsh2vVA/ODw71D1VuUE.LY73K/GK2CnBmKTXIE9Y4XW3zt3vfWy','王','大大',921123456,0),(5,'test1@gmail.com','$2y$10$tcL8Mh7dsS5KkdHJV4NM4.IGBBoNP2ZxCiqXFHQhICIurquXKNxMO','張','小小',912345678,0),(6,'test2@gmail.com','$2y$10$TBnp2eeAPWgythSHdXlXfOk.IKiMFJA4/YqGnuQEMKoJzPSSFlcL6','廖','中中',934678901,0),(7,'test3@gmail.com','$2y$10$yPMUGemtN4yJqWxD7NzK4.fN/5LMsxyh.jPYqExmnE1UTlmQPolj6','楊','津津',988641234,0);
+INSERT INTO `usermember` VALUES (11,'test9@gmail.com','$2y$10$PXQi1/dsM4XJ6hf121rcYePZej0sMv3oSDlXs4k11KCr9VzaTTvZW','楊','淘淘','0954367890',0);
 /*!40000 ALTER TABLE `usermember` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,4 +90,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-06  5:00:45
+-- Dump completed on 2022-07-07 17:06:25

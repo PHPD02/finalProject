53dda9e0ca885d17edd460fa9944370ee0a8a275
chatty <bisa8002@gmail.com>
2022-07-11 21:32:58 +0800

    storecards

--- project/src/chattyComponents/components item/storeCards.jsx
+++ project/src/chattyComponents/components item/storeCards.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import "../css/storeCard.css";
-class Cards extends Component {
+class ShopCard extends Component {
   state = {}
   render() {
     return (
       <React.Fragment>
-        <NavLink to="/shopList" target="_blank">
-          <div className="card setHeight">
-            {/* 需要套資料庫呈現(圖片) */}
-            <svg className="bd-img bd-img-lg featurette-image img-fluid mx-auto" id="myImg" width="100%" height="200" xmlns="http://www.w3.org/2000/svg"><rect width="100%" height="100%" fill="#F08080"></rect></svg>
-            <div>
-              <div className='row'>
-                {/* 店家名稱 */}
-                <div className='col-6'><b>店家名稱</b></div>
-                {/* 店家的評價★ */}
-                <div className='col-6'>
-                  <i className="fa fa-star" style={{ color: "red" }}></i>
-                  <span className="star-point">5</span> /
-                  <span className="comment-amount">5</span>
+        <div className='card' style={{ boxShadow: "3px 3px 3px grey" }}>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-12 col-sm-12'>
+                {/* 圖片 */}
+                <svg className="bd-img bd-img-lg featurette-image img-fluid rounded float-left" width="100" height="100" xmlns="http://www.w3.org/2000/svg"><rect width="100%" height="100%" fill="#eee" /><text x="25%" y="25%" fill="#aaa">100x100</text></svg>
+                <div className='row m-2'>
+                  <div className='col'>
+                    {/* 商品名稱 */}
+                    <h4>奶茶布丁</h4>
+                    <br />
+                    {/* 金額 */}
+                    <span>$<span>55</span></span>
+                  </div>
+
+                  <button type="button" className='mt-4 btn btn-primary rounded-lg'>+</button>
                 </div>
+
               </div>
-              <br />
-              {/* 餐廳的種類 */}
-              <small className="restaurant-type">$$$,<b>種類</b></small>
+
+
 
             </div>
           </div>
-        </NavLink>
+        </div>
+
 
 
       </React.Fragment>
@@ -35,4 +37,4 @@ class Cards extends Component {
   }
 }
 
-export default Cards;
+export default ShopCard;

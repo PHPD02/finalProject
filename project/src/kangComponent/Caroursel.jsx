abf61f4196699ba8e11ee92339a097d5f9472de0
alex <bgkong1205@gmail.com>
2022-07-11 13:35:08 +0800

    foodpanda業修改

--- project/src/kangComponent/Caroursel.jsx
+++ project/src/kangComponent/Caroursel.jsx
@@ -12,10 +12,10 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-import img1 from './img1.jpg';
-import img2 from './img2.jpg';
-import img3 from './img3.jpg';
-import img4 from './img4.jpg';
+import img1 from '../img/img1.jpg';
+import img2 from '../img/img2.jpg';
+import img3 from '../img/img3.jpg';
+import img4 from '../img/img4.jpg';
 
 
 
@@ -38,7 +38,7 @@ class Caroursel extends React.Component {
               pagination={{ clickable: true }}
               onSwiper={(swiper) => console.log(swiper)}
               // onSlideChange={() => console.log('slide change')}
-              style={{width:'100%',height:'60vh'}}
+              style={{width:'100%',height:'70vh'}}
             >
               <SwiperSlide><img src={img1} alt="img1" style={{objectFit:'auto'}}/></SwiperSlide>
               <SwiperSlide><img src={img2} alt="img2" style={{objectFit:'auto'}}/></SwiperSlide>

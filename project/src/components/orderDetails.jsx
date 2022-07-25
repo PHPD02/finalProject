05dd34932a8270bdad52f7fa9c912d748acb4548
Rjie <linjj0709@gmail.com>
2022-07-25 10:39:01 +0800

    更新訂單詳情頁 資料庫目前還沒併其他表單 ))

--- project/src/components/orderDetails.jsx
+++ project/src/components/orderDetails.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './repeatability/Navbar';
 import './css/orderdetails.css';
 import axios from 'axios';
+import DetailsCards from './component/DetailsCards';
 // import Footer from '../repeatability/Footer';
 class OrderDetails extends Component {
   state = {
@@ -12,16 +13,11 @@ class OrderDetails extends Component {
   componentDidMount = async () => {
     let url = "http://localhost/ourPHPFinalproject/RjieProject/details.php"
     await axios.get(url)
-      .then((res) => {
+      .then(res => {
         this.state.details = res.data;
-        // this.state.details.forEach((value, index) => {
-        //   console.log(value)
-        // })
-
         this.setState({});
       })
-    // console.log(this.state.details);
-    // console.log(this.state.orderId);
+      // console.log(this.state.details);
   }
 
   render() {
@@ -58,62 +54,26 @@ class OrderDetails extends Component {
                   <h4>餐點準備中</h4>
                 </div>
               </div>
-
-              {this.state.details.map((value, index) => {
-                return (
-                  <>
-                    <div className='col-md-6 col-sm-12 container' key={index}>
-                      <div className='card'>
-                        {/* {this.state.details.map(
-                    (detail, index) => {
-                      return (
-                        <> */}
-                        <h3>訂單詳情</h3>
-                        <br />
-                        <h5>訂單編號:{value.orderId}</h5>
-                        <br />
-                        <h5>訂單配送來自:{ }</h5>
-                        <br />
-                        <h5>送餐地址:{ }</h5>
-                        <br />
-                        <h5>總計: ${ }</h5>
-                        {/* 撈資料庫產生 */}
-                        <h5>查看細節({ }個品項):
-                          <span className="dropdown">
-                            <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                              <i className="dropdown-item">{ }</i>
-                              <i className="dropdown-item"></i>
-                            </div>
-                          </span></h5>
-                        {/* </>);
-                    })} */}
-                        {/* 選擇器跑出來的內容 */}
-                      </div>
-                      <div className='card'>
-                        <div className='col-12'>
-                          <h4>需要幫助嗎 ?</h4>
-                          <h6>有需要的話聯絡我們</h6>
-                          <button className="btn btn-outline-danger">客服中心</button>
-                        </div>
-                      </div>
-                    </div>
-
-                  </>
-                )
-              })}
-
-
-
-
+              <div className='col-md-6 col-sm-12 container' >
+                <div className='card'>
+                  <h3>訂單詳情</h3>
+                  {this.state.details.map(
+                    (details, index) => {
+                      return (<DetailsCards key={index} catchDetails={details} />);
+                    })}
+                  {/* 選擇器跑出來的內容 */}
+                </div>
+                <div className='card'>
+                  <div className='col-12'>
+                    <h4>需要幫助嗎 ?</h4>
+                    <h6>有需要的話聯絡我們</h6>
+                    <button className="btn btn-outline-danger">客服中心</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-
-
-
-
       </React.Fragment>
     );
   }

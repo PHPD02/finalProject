5c030bb9316925f31199c6c2e48724eafc909175
RJie <linjj0709@gmail.com>
2022-07-26 08:06:30 +0800

    撈取最新一筆資料

--- project/public/php/details.php
+++ project/public/php/details.php
@@ -1,7 +1,8 @@
 <?php
 include('serverHeader.php');
 include('sql.php');
-$sql = "SELECT * FROM orderdetails WHERE 1";
+// INNER JOIN 內部連接
+$sql = "SELECT *,ROW_NUMBER() OVER(ORDER BY orderId DESC) sn FROM orderdetails LIMIT 1";
 
 $result = $mysqli->query($sql);
 

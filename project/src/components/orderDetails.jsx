41a4aff1999bc830a58eb5ec569d3c44df077c01
RJie <linjj0709@gmail.com>
2022-07-27 08:20:54 +0800

    時間設置 還未完成   刪除圖片 和新增 部分圖片

--- project/src/components/orderDetails.jsx
+++ project/src/components/orderDetails.jsx
@@ -3,16 +3,30 @@ import Navbar from './repeatability/Navbar';
 import './css/orderdetails.css';
 // import axios from 'axios';
 import DetailsCards from './component/DetailsCards';
-// import Footer from '../repeatability/Footer';
+import Footer from './repeatability/Footer';
 import FirebaseIndex from '../kangComponent/firebase/firebaseIndex';
 class OrderDetails extends Component {
-  state = {}
-
-
-  componentDidMount = async () => {
+  state = {
+    date: new Date(),
+    date1: new Date(),
+    data2: '',
   }
 
+  componentDidMount = () => {
+    this.timerId = setInterval(() => {
+      this.setState({ date: new Date() })
+    }, 1000)
+
+    // console.log(this.state.date);
+    // console.log(this.state.date1);
+    this.state.data2 = this.state.date - this.state.date1 + 1;
+    console.log(this.state.data2)
+  }
+
+
+
   render() {
+
     return (
       <React.Fragment>
         <Navbar />
@@ -23,15 +37,20 @@ class OrderDetails extends Component {
               <div className='col-md-6 col-sm-12 container'>
                 <div className='card'>
                   <div className='row'>
-                    <div className='col-12 '>
-                      <h4>預計外送時間</h4>
+                    <div className='col-11 text-center'>
+                      <h4>預計外送時間:{this.state.data2}分鐘</h4>
+                      {this.state.date.getHours()}時
+                      {this.state.date.getMinutes()}分
+                      {this.state.date.getSeconds()}秒-
+                      {this.state.date1.getHours()}:時
+                      {this.state.date1.getMinutes() + 20}分
                     </div>
                     {/* 讀取外送時間 */}
                     <div className='col-12'>
-                      <p className='display-3'>{ }</p>
+                      <p className='display-3'></p>
                     </div>
                     <div className='col-12'>
-                      <img src="./image/deliveryman.png" />
+                      <div className='thispic'></div>
                     </div>
                     {/* <div className='col-12 row'>
                       <p className="progress-1"></p>
@@ -40,16 +59,17 @@ class OrderDetails extends Component {
                       <p className="progress-4"></p>
                     </div> */}
                   </div>
-                  <br />
-                  <br />
                   {/* 再送達後更改內容 */}
-                  <h4>餐點準備中</h4>
+                  <div className='col-11 text-center'>
+                    <h4>{this.state.data2 == 0 ? "餐點已送達!謝謝光臨" : "餐點準備中"}</h4>
+
+                  </div>
                 </div>
               </div>
               <div className='col-md-6 col-sm-12 container' >
                 <div className='card'>
                   <h3>訂單詳情</h3>
-                    <DetailsCards/>
+                  <DetailsCards />
                   {/* 選擇器跑出來的內容 */}
                   <br />
                   <FirebaseIndex />
@@ -70,6 +90,7 @@ class OrderDetails extends Component {
             </div>
           </div>
         </div>
+        <Footer />
       </React.Fragment>
     );
   }

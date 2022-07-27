6d9469ec8e79aa72b5df8a5e1dee6d1235917730
Rjie <linjj0709@gmail.com>
2022-07-27 17:30:47 +0800

    footer 修改

--- project/src/components/repeatability/Footer.jsx
+++ project/src/components/repeatability/Footer.jsx
@@ -23,24 +23,21 @@ class Footer extends Component {
         <div className="footerbackimg" id='ff'>
           <div className='container'>
             <div className='row'>
-              <div className='col-4 col-sm-4 col-md-4 col-lg-4 mt-5 text-center'>
+              <div className='col-4 col-sm-4 col-md-4 col-lg-4 mt-1 text-center'>
                 <h5>關於</h5>
-                <hr />
                 <li><a href='#'>外送夥伴品牌形象</a></li>
                 <li><a href='#'>承攬服務條款</a></li>
                 <li><a href='#'>隱私權政策</a></li>
               </div>
-              <div className='col-4 col-sm-4 col-md-4 col-lg-4 mt-5 text-center'>
+              <div className='col-4 col-sm-4 col-md-4 col-lg-4 mt-1 text-center'>
                 <h5 className='text-center'>取得協助</h5>
-                <hr />
                 <li><a href='#'>常見Q&A</a></li>
                 <li><a href='#'>營運中心開放時間</a></li>
               </div>
-              <div className='col-4 col-sm-3 col-md-3 col-lg-3 mt-5 text-center'>
+              <div className='col-4 col-sm-4 col-md-4 col-lg-4 mt-1 text-center'>
                 <h5 className='text-center'>請聯絡我們</h5>
-                <hr />
                 {/* id="emails"onMouseEnter={this.openChange} onMouseLeave={this.openChange}  */}
-                <i className="fa fa-envelope-o fa-5x"></i>
+                <i className="fa fa-envelope-o fa-3x"></i>
               </div>
             </div>
           </div>
